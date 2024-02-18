7bbd4a6c121eefefe34871bd7d1307d434cc66e8
lagerros <jacobklagerros@gmail.com>
2024-02-18 08:50:19 +0000

    change python version

---
+++ add_mcmaster_carr_to_database.py
@@ -0,0 +1,9 @@
+
+
+# query the  mc master api
+# fetch the parts
+# for each part, call 
+
+##with app.app_context():
+  print(name, description, img_file_name, step_file_name)
+ # createPart(name, description, img_file_name, stl_file_name)
