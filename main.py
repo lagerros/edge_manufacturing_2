25a6c21c9f9a99326a7bcf9245ab3cb1a3610428
lagerros <jacobklagerros@gmail.com>
2024-02-18 01:49:49 -0800

    experiment with socket

--- main.py
+++ main.py
@@ -114,3 +114,21 @@ def get_part(part_id):
 
 
 app.run(host='0.0.0.0', port=8080)
+
+from flask import Flask, request
+from flask_sockets import Sockets
+
+# app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
